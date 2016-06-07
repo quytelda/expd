e85d89c2dd0a4f042ee382a98438ad057c349d9a
Quytelda Kahja <quytelda@tamalin.org>
2016-06-06 22:59:33 -0400

    Move protocol sources to /exp directory and add headers for clients and messages.

---
+++ exp/message.h
@@ -0,0 +1,35 @@
+/* message.h - EXP messages and message parsing
+ * Copyright (C) 2016 Quytelda Kahja
+ *
+ * This file is part of EXPd.
+ * 
+ * EXPd is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * EXPd is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with EXPd.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __EXP_MESSAGE_H
+#define __EXP_MESSAGE_H
+
+struct exp_message
+{
+    char * source;
+    char * command;
+
+    int argc;
+    char * argv[];
+};
+
+struct exp_message * parse_message(char * msg);
+char * message_tostr(struct exp_message *);
+
+#endif /* __EXP_MESSAGE_H */
